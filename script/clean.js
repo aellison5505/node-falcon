bdea381115279c841d54eb96a6f526e7949a6cda
jbfd-ems <25670817+aellison5505@users.noreply.github.com>
2020-09-28 20:12:43 -0400

    fix lib

--- script/clean.js
+++ script/clean.js
@@ -3,10 +3,10 @@ const { unlinkSync, readdirSync } = require('fs');
 let main = async () => {
     // await copyFile();
     try{
-    let files = readdirSync('addon');
+    let files = readdirSync('libaddon');
     files.forEach((item) => {
         console.log(item);
-         unlinkSync(`addon/${item}`);
+         unlinkSync(`libaddon/${item}`);
     });
      } catch (err) {
          console.log('No files to moved!', err);
--- script/move.js
+++ script/move.js
@@ -6,7 +6,7 @@ let main = async () => {
    let files = readdirSync('build/Release');
    files.forEach((item) => {
        console.log(item);
-        copyFileSync(`build/Release${item}`,`addon/${item}`);
+        copyFileSync(`build/Release${item}`,`libaddon/${item}`);
    });
     } catch (err) {
         console.log('No files to move', err);
